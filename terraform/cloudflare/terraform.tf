3e211e46195be6bea69b47879568d68c80b35e09
tomtucka <tomtucka@gmail.com>
2022-01-03 01:00:54 +0000

    Cloudflare terraform

---
+++ terraform/cloudflare/terraform.tf
@@ -0,0 +1,22 @@
+terraform {
+
+  required_providers {
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "3.6.0"
+    }
+    http = {
+      source  = "hashicorp/http"
+      version = "2.1.0"
+    }
+    sops = {
+      source  = "carlpett/sops"
+      version = "0.6.3"
+    }
+  }
+}
+
+provider "cloudflare" {
+  email   = data.sops_file.cloudflare_secrets.data["cloudflare_email"]
+  api_key = data.sops_file.cloudflare_secrets.data["cloudflare_apikey"]
+}
